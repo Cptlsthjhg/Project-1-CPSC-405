1e285b5b3477f9d72d0cac566979113fe0e022d0
Ethan Thompson <ethan.m.thompson@gmail.com>
2023-02-15 20:20:49 +0000

    tmod function works! :)

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 /*****************************************************************
-*    main.c - Gusty - began with code from https://codereview.stackexchange.com/questions/67746/simple-shell-in-c
-********************************************************************/
+ *    main.c - Gusty - began with code from https://codereview.stackexchange.com/questions/67746/simple-shell-in-c
+ ********************************************************************/
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -20,8 +20,8 @@ int executeCmd(char** params, int nparams);
 #define MAX_COMMAND_LENGTH 100
 #define MAX_NUMBER_OF_PARAMS 10
 
-enum cmds        { FORK=0, SETPID,   SHOWPID,   WAIT,   EXIT,   SLEEP,   WAKEUP,   PS,   SCHEDULE,  TIMER,    HELP,   QUIT };
-char *cmdstr[] = {"fork", "Setpid", "currpid",  "wait", "exit", "sleep", "wakeup", "ps",   "schedule", "timer", "help", "quit"};
+enum cmds        { FORK=0, SETPID,   SHOWPID,   WAIT,   EXIT,   SLEEP,   WAKEUP,   PS,   SCHEDULE,  TIMER,    HELP,   QUIT ,  TMOD};
+char *cmdstr[] = {"fork", "Setpid", "currpid",  "wait", "exit", "sleep", "wakeup", "ps",   "schedule", "timer", "help", "quit", "tmod"};
 
 int curr_proc_id = 0;
 
@@ -76,97 +76,115 @@ int executeCmd(char** params, int nparams)
             break;
 
     //for (int i = 0; i < nparams; i++)
-        //printf("Param %d: %s\n", i, params[i]);
+    //printf("Param %d: %s\n", i, params[i]);
     //printf("ncmds: %d, cmd_index: %d\n", ncmds, cmd_index);
-    
+
     switch (cmd_index) {
-    case FORK:
-        if (nparams > 1)
-            pid = atoi(params[1]);
-        else
-            pid = curr_proc->pid;
-        int fpid = Fork(pid);
-        printf("pid: %d forked: %d\n", pid, fpid);
-        break;
-    case SETPID:
-        if (nparams == 1)
-            printf("setpid cmd requires pid parameter\n");
-        else
-            curr_proc_id = atoi(params[1]);
-        break;
-    case SHOWPID:
-        //printf("Current pid: %d\n", curr_proc_id);
-        printf("Current pid: %d\n", curr_proc->pid);
-        break;
-    case WAIT:
-        if (nparams > 1)
-            pid = atoi(params[1]);
-        else
-            pid = curr_proc->pid;
-        int wpid = Wait(pid);
-        if (wpid == -1)
-            printf("pid: %d has no children to wait for.\n", pid);
-        else if (wpid == -2)
-            printf("pid: %d has children, but children still running.\n", pid);
-        else
-            printf("pid: %d child %d has terminated.\n", pid, wpid);
-        break;
-    case EXIT:
-        if (nparams > 1)
-            pid = atoi(params[1]);
-        else
-            pid = curr_proc->pid;
-        pid = Exit(pid);
-        printf("Exit Status:: %d .\n", pid);
-        break;
-    case SLEEP:
-        if (nparams < 2)
-            printf("Sleep chan [pid]\n");
-        else {
-            chan = atoi(params[1]);
-            if (nparams > 2)
-                pid = atoi(params[2]);
+        case FORK:
+            if (nparams > 1)
+                pid = atoi(params[1]);
             else
                 pid = curr_proc->pid;
-            pid = Sleep(pid, chan);
-            printf("Sleep Status:: %d .\n", pid);
-        }
-        break;
-    case WAKEUP:
-        if (nparams < 2)
-            printf("Wakeup chan\n");
-        else {
-            chan = atoi(params[1]);
-            Wakeup(chan);
-        }
-        break;
-    case PS:
-        procdump();
-        break;
-    case SCHEDULE:
-        pid = local_scheduler();
-        printf("Scheduler selected pid: %d\n", pid);
-        break;
-    case TIMER:
-        if (nparams < 2)
-            printf("timer quantums\n");
-        else {
-            int quantums = atoi(params[1]);
-            for (int i = 0; i < quantums; i++) {
-                pid = local_scheduler();
-                printf("Scheduler selected pid: %d\n", pid);
+            int fpid = Fork(pid);
+            printf("pid: %d forked: %d\n", pid, fpid);
+            break;
+        case TMOD:
+            if(nparams > 2){
+                pid = atoi(params[1]);
+                int ticketValue = atoi(params[2]);
+                int oldTicketValue = Tmod(pid,ticketValue);
+                if(oldTicketValue==-1){
+                    //tell user that the Tmod failed to find process
+                    printf("Failed to find process based off of PID\n");
+                }
+                else{
+                    //tell user that Tmod worked, and what happened.
+                    printf("Process %d had %d tickets, now has %d tickets.\n",pid,oldTicketValue,ticketValue);
+                }
             }
-        }
-        break;
-    case HELP:
-        printf("Commands: fork, wait, exit, ps, Setpid, currpid, sleep, wakeup, timer, help\n");
-        break;
-    case QUIT:
-        rc = 0;
-        break;
-    default:
-        printf("Invalid command! Enter Help to see commands.\n");
+            else{
+                printf("Invalid TMOD usage (parameter 1 is PID, and parameter 2 is ticket value.\n)");
+                break;
+            }
+        case SETPID:
+            if (nparams == 1)
+                printf("setpid cmd requires pid parameter\n");
+            else
+                curr_proc_id = atoi(params[1]);
+            break;
+        case SHOWPID:
+            //printf("Current pid: %d\n", curr_proc_id);
+            printf("Current pid: %d\n", curr_proc->pid);
+            break;
+        case WAIT:
+            if (nparams > 1)
+                pid = atoi(params[1]);
+            else
+                pid = curr_proc->pid;
+            int wpid = Wait(pid);
+            if (wpid == -1)
+                printf("pid: %d has no children to wait for.\n", pid);
+            else if (wpid == -2)
+                printf("pid: %d has children, but children still running.\n", pid);
+            else
+                printf("pid: %d child %d has terminated.\n", pid, wpid);
+            break;
+        case EXIT:
+            if (nparams > 1)
+                pid = atoi(params[1]);
+            else
+                pid = curr_proc->pid;
+            pid = Exit(pid);
+            printf("Exit Status:: %d .\n", pid);
+            break;
+        case SLEEP:
+            if (nparams < 2)
+                printf("Sleep chan [pid]\n");
+            else {
+                chan = atoi(params[1]);
+                if (nparams > 2)
+                    pid = atoi(params[2]);
+                else
+                    pid = curr_proc->pid;
+                pid = Sleep(pid, chan);
+                printf("Sleep Status:: %d .\n", pid);
+            }
+            break;
+        case WAKEUP:
+            if (nparams < 2)
+                printf("Wakeup chan\n");
+            else {
+                chan = atoi(params[1]);
+                Wakeup(chan);
+            }
+            break;
+        case PS:
+            procdump();
+            break;
+        case SCHEDULE:
+            pid = local_scheduler();
+            printf("Scheduler selected pid: %d\n", pid);
+            break;
+        case TIMER:
+            if (nparams < 2)
+                printf("timer quantums\n");
+            else {
+                int quantums = atoi(params[1]);
+                for (int i = 0; i < quantums; i++) {
+                    pid = local_scheduler();
+                    printf("Scheduler selected pid: %d\n", pid);
+                }
+            }
+            break;
+        case HELP:
+            printf("Commands: fork, wait, exit, ps, Setpid, currpid, sleep, wakeup, timer, help\n");
+            break;
+        case QUIT:
+            rc = 0;
+            break;
+        default:
+            printf("Invalid command! Enter Help to see commands.\n");
     }
-    
+
     return rc;
 }
--- proc.c
+++ proc.c
@@ -1,7 +1,7 @@
 /*****************************************************************
-*       proc.c - simplified for CPSC405 Lab by Gusty Cooper, University of Mary Washington
-*       adapted from MIT xv6 by Zhiyi Huang, hzy@cs.otago.ac.nz, University of Otago
-********************************************************************/
+ *       proc.c - simplified for CPSC405 Lab by Gusty Cooper, University of Mary Washington
+ *       adapted from MIT xv6 by Zhiyi Huang, hzy@cs.otago.ac.nz, University of Otago
+ ********************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +9,7 @@
 #include "types.h"
 #include "defs.h"
 #include "proc.h"
+
 
 static void wakeup1(int chan);
 
@@ -26,8 +27,8 @@ char *procstatep[] = { "UNUSED", "EMPRYO", "SLEEPING", "RUNNABLE", "RUNNING", "Z
 
 // Table of all processes
 struct {
-  int lock;   // not used in Lab
-  struct proc proc[NPROC];
+    int lock;   // not used in Lab
+    struct proc proc[NPROC];
 } ptable;
 
 // Initial process - ascendent of all other processes
@@ -37,236 +38,253 @@ static struct proc *initproc;
 int nextpid = 1;
 
 // Funtion to use as address of proc's PC
-void
+    void
 forkret(void)
 {
 }
 
 // Funtion to use as address of proc's LR
-void
+    void
 trapret(void)
 {
 }
 
 // Initialize the process table
-void
+    void
 pinit(void)
 {
-  memset(&ptable, 0, sizeof(ptable));
+    memset(&ptable, 0, sizeof(ptable));
 }
 
 // Look in the process table for a process id
 // If found, return pointer to proc
 // Otherwise return 0.
-static struct proc*
+    static struct proc*
 findproc(int pid)
 {
-  struct proc *p;
+    struct proc *p;
 
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->pid == pid)
-      return p;
-  return 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+        if(p->pid == pid)
+            return p;
+    return 0;
 }
 
 // Look in the process table for an UNUSED proc.
 // If found, change state to EMBRYO and initialize
 // state required to run in the kernel.
 // Otherwise return 0.
-static struct proc*
+    static struct proc*
 allocproc(void)
 {
-  struct proc *p;
+    struct proc *p;
 
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == UNUSED)
-      goto found;
-  return 0;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+        if(p->state == UNUSED)
+            goto found;
+    return 0;
 
 found:
-  p->state = EMBRYO;
-  p->pid = nextpid++;
+    p->state = EMBRYO;
+    p->pid = nextpid++;
 
-  p->tickets = 10;
+    p->tickets = 10;
 
-  p->context = (struct context*)malloc(sizeof(struct context));
-  memset(p->context, 0, sizeof *p->context);
-  p->context->pc = (uint)forkret;
-  p->context->lr = (uint)trapret;
+    p->context = (struct context*)malloc(sizeof(struct context));
+    memset(p->context, 0, sizeof *p->context);
+    p->context->pc = (uint)forkret;
+    p->context->lr = (uint)trapret;
 
-  return p;
+    return p;
 }
 
 // Set up first user process.
-int
+    int
 userinit(void)
 {
-  struct proc *p;
-  p = allocproc();
-  initproc = p;
-  p->sz = PGSIZE;
-  strcpy(p->cwd, "/");
-  strcpy(p->name, "userinit"); 
-  p->state = RUNNING;
+    struct proc *p;
+    p = allocproc();
+    initproc = p;
+    p->sz = PGSIZE;
+    strcpy(p->cwd, "/");
+    strcpy(p->name, "userinit"); 
+    p->state = RUNNING;
 
-  //We're going to assign the firs user process some tickets too:
-  p->tickets = 10;
+    //We're going to assign the firs user process some tickets too:
+    p->tickets = 10;
 
-  curr_proc = p;
-  return p->pid;
+    curr_proc = p;
+    return p->pid;
 }
 
 // Create a new process copying p as the parent.
 // Sets up stack to return as if from system call.
 // Caller must set state of returned proc to RUNNABLE.
-int
+    int
 Fork(int fork_proc_id)
 {
-  int pid;
-  struct proc *np, *fork_proc;
+    int pid;
+    struct proc *np, *fork_proc;
 
-  // Find current proc
-  if ((fork_proc = findproc(fork_proc_id)) == 0)
-    return -1;
+    // Find current proc
+    if ((fork_proc = findproc(fork_proc_id)) == 0)
+        return -1;
 
-  // Allocate process.
-  if((np = allocproc()) == 0)
-    return -1;
+    // Allocate process.
+    if((np = allocproc()) == 0)
+        return -1;
 
-  // Copy process state from p.
-  np->sz = fork_proc->sz;
-  np->parent = fork_proc;
-  // Copy files in real code
-  strcpy(np->cwd, fork_proc->cwd);
- 
-  pid = np->pid;
-  np->state = RUNNABLE;
-  strcpy(np->name, fork_proc->name);
-  return pid;
+    // Copy process state from p.
+    np->sz = fork_proc->sz;
+    np->parent = fork_proc;
+    // Copy files in real code
+    strcpy(np->cwd, fork_proc->cwd);
+
+    pid = np->pid;
+    np->state = RUNNABLE;
+    strcpy(np->name, fork_proc->name);
+    return pid;
 }
 
+int Tmod(int pid, int ticketValue){
+
+    struct proc *p;
+
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->pid==pid){
+
+            int oldTickets = p->tickets;
+            p->tickets = ticketValue;
+            return oldTickets;
+        }
+    }
+    release(&ptable.lock);
+    return -1;
+
+}
 // Exit the current process.  Does not return.
 // An exited process remains in the zombie state
 // until its parent calls wait() to find out it exited.
-int
+    int
 Exit(int exit_proc_id)
 {
-  struct proc *p, *exit_proc;
+    struct proc *p, *exit_proc;
 
-  // Find current proc
-  if ((exit_proc = findproc(exit_proc_id)) == 0)
-    return -2;
+    // Find current proc
+    if ((exit_proc = findproc(exit_proc_id)) == 0)
+        return -2;
 
-  if(exit_proc == initproc) {
-    printf("initproc exiting\n");
-    return -1;
-  }
-
-  // Close all open files of exit_proc in real code.
-
-  acquire(&ptable.lock);
-
-  wakeup1(exit_proc->parent->pid);
-
-  // Place abandoned children in ZOMBIE state - HERE
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p->parent == exit_proc){
-      p->parent = initproc;
-      p->state = ZOMBIE;
+    if(exit_proc == initproc) {
+        printf("initproc exiting\n");
+        return -1;
     }
-  }
 
-  exit_proc->state = ZOMBIE;
+    // Close all open files of exit_proc in real code.
 
-  // sched();
-  release(&ptable.lock);
-  return 0;
+    acquire(&ptable.lock);
+
+    wakeup1(exit_proc->parent->pid);
+
+    // Place abandoned children in ZOMBIE state - HERE
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->parent == exit_proc){
+            p->parent = initproc;
+            p->state = ZOMBIE;
+        }
+    }
+
+    exit_proc->state = ZOMBIE;
+
+    // sched();
+    release(&ptable.lock);
+    return 0;
 }
 
 // Wait for a child process to exit and return its pid.
 // Return -1 if this process has no children.
 // Return -2 has children, but not zombie - must keep waiting
 // Return -3 if wait_proc_id is not found
-int
+    int
 Wait(int wait_proc_id)
 {
-  struct proc *p, *wait_proc;
-  int havekids, pid;
+    struct proc *p, *wait_proc;
+    int havekids, pid;
 
-  // Find current proc
-  if ((wait_proc = findproc(wait_proc_id)) == 0)
-    return -3;
+    // Find current proc
+    if ((wait_proc = findproc(wait_proc_id)) == 0)
+        return -3;
 
-  acquire(&ptable.lock);
-  for(;;){ // remove outer loop
-    // Scan through table looking for zombie children.
-    havekids = 0;
-    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-      if(p->parent != wait_proc)
-        continue;
-      havekids = 1;
-      if(p->state == ZOMBIE){
-        // Found one.
-        pid = p->pid;
-        p->kstack = 0;
-        p->state = UNUSED;
-        p->pid = 0;
-        p->parent = 0;
-        p->name[0] = 0;
-        p->killed = 0;
-        release(&ptable.lock);
-        return pid;
-      }
+    acquire(&ptable.lock);
+    for(;;){ // remove outer loop
+        // Scan through table looking for zombie children.
+        havekids = 0;
+        for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+            if(p->parent != wait_proc)
+                continue;
+            havekids = 1;
+            if(p->state == ZOMBIE){
+                // Found one.
+                pid = p->pid;
+                p->kstack = 0;
+                p->state = UNUSED;
+                p->pid = 0;
+                p->parent = 0;
+                p->name[0] = 0;
+                p->killed = 0;
+                release(&ptable.lock);
+                return pid;
+            }
+        }
+
+        // No point waiting if we don't have any children.
+        if(!havekids || wait_proc->killed){
+            release(&ptable.lock);
+            return -1;
+        }
+        if (havekids) { // children still running
+            Sleep(wait_proc_id, wait_proc_id);
+            release(&ptable.lock);
+            return -2;
+        }
+
     }
-
-    // No point waiting if we don't have any children.
-    if(!havekids || wait_proc->killed){
-      release(&ptable.lock);
-      return -1;
-    }
-    if (havekids) { // children still running
-      Sleep(wait_proc_id, wait_proc_id);
-      release(&ptable.lock);
-      return -2;
-    }
-
-  }
 }
 
 // Atomically release lock and sleep on chan.
 // Reacquires lock when awakened.
-int
+    int
 Sleep(int sleep_proc_id, int chan)
 {
-  struct proc *sleep_proc;
-  // Find current proc
-  if ((sleep_proc = findproc(sleep_proc_id)) == 0)
-    return -3;
+    struct proc *sleep_proc;
+    // Find current proc
+    if ((sleep_proc = findproc(sleep_proc_id)) == 0)
+        return -3;
 
-  sleep_proc->chan = chan;
-  sleep_proc->state = SLEEPING;
-  return sleep_proc_id;
+    sleep_proc->chan = chan;
+    sleep_proc->state = SLEEPING;
+    return sleep_proc_id;
 }
 
 // Wake up all processes sleeping on chan.
 // The ptable lock must be held.
-static void
+    static void
 wakeup1(int chan)
 {
-  struct proc *p;
+    struct proc *p;
 
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->state == SLEEPING && p->chan == chan)
-      p->state = RUNNABLE;
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+        if(p->state == SLEEPING && p->chan == chan)
+            p->state = RUNNABLE;
 }
 
 
-void
+    void
 Wakeup(int chan)
 {
-  acquire(&ptable.lock);
-  wakeup1(chan);
-  release(&ptable.lock);
+    acquire(&ptable.lock);
+    wakeup1(chan);
+    release(&ptable.lock);
 }
 
 
@@ -274,23 +292,23 @@ Wakeup(int chan)
 // Kill the process with the given pid.
 // Process won't exit until it returns
 // to user space (see trap in trap.c).
-int
+    int
 Kill(int pid)
 {
-  struct proc *p;
+    struct proc *p;
 
-  acquire(&ptable.lock);
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p->pid == pid){
-      p->killed = 1;
-      // Wake process from sleep if necessary.
-      if(p->state == SLEEPING)
-        p->state = RUNNABLE;
-      return 0;
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p->pid == pid){
+            p->killed = 1;
+            // Wake process from sleep if necessary.
+            if(p->state == SLEEPING)
+                p->state = RUNNABLE;
+            return 0;
+        }
     }
-  }
-  release(&ptable.lock);
-  return -1;
+    release(&ptable.lock);
+    return -1;
 }
 
 // Per-CPU process scheduler.
@@ -300,42 +318,42 @@ Kill(int pid)
 //  - swtch to start running that process
 //  - eventually that process transfers control
 //      via swtch back to the scheduler.
-void
+    void
 scheduler(void)
 {
-// A continous loop in real code
-//  if(first_sched) first_sched = 0;
-//  else sti();
+    // A continous loop in real code
+    //  if(first_sched) first_sched = 0;
+    //  else sti();
 
-  curr_proc->state = RUNNABLE;
+    curr_proc->state = RUNNABLE;
 
-  struct proc *p;
+    struct proc *p;
 
-  acquire(&ptable.lock);
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
-    if(p == curr_proc || p->state != RUNNABLE)
-      continue;
+    acquire(&ptable.lock);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+        if(p == curr_proc || p->state != RUNNABLE)
+            continue;
 
-    // Switch to chosen process.
-    curr_proc = p;
-    p->state = RUNNING;
-    break;
-  }
-  release(&ptable.lock);
+        // Switch to chosen process.
+        curr_proc = p;
+        p->state = RUNNING;
+        break;
+    }
+    release(&ptable.lock);
 
 }
 
 // Print a process listing to console.  For debugging.
 // Runs when user types ^P on console.
 // No lock to avoid wedging a stuck machine further.
-void
+    void
 procdump(void)
 {
-  struct proc *p;
+    struct proc *p;
 
-  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
-    if(p->pid > 0)
-      printf("pid: %d, parent: %d state: %s tickets: %d\n", p->pid, p->parent == 0 ? 0 : p->parent->pid, procstatep[p->state], p->tickets);
+    for(p = ptable.proc; p < &ptable.proc[NPROC]; p++)
+        if(p->pid > 0)
+            printf("pid: %d, parent: %d state: %s tickets: %d\n", p->pid, p->parent == 0 ? 0 : p->parent->pid, procstatep[p->state], p->tickets);
 }
 
 
